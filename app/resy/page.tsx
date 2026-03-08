5c1cf3caee4bda2a51f22522d4ac067ec89e84d8
Chris Mueller <ccmueller92@gmail.com>
2026-03-07 21:34:17 -0500

    chore: update wording on resy scraper page

--- app/resy/page.tsx
+++ app/resy/page.tsx
@@ -5,9 +5,9 @@ export default function ResyPage() {
     return (
         <div className="max-w-4xl mx-auto">
             <header className="mb-12 border-b pb-4">
-                <h1 className="text-4xl font-bold tracking-tight">Resy Reservation Finder</h1>
+                <h1 className="text-4xl font-bold tracking-tight">Resy Scraper</h1>
                 <p className="text-gray-500 mt-2 text-lg">
-                    Add custom email alerts for any Resy restaurant. The scraper runs automatically every 5 minutes and will notify the specified email addresses!
+                    The scraper runs every minute and will notify the email address at most once for each availabiilty.
                 </p>
             </header>
             <div className="w-full">
